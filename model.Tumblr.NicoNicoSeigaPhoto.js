b16653a17cdaf6a93e983b92acb32c0588d5f79f
asiamoth <asiamoth@gmail.com>
2011-05-01 00:04:05 +0900

    「ニコニコ静画」用のパッチを追加

---
+++ model.Tumblr.NicoNicoSeigaPhoto.js
@@ -0,0 +1,17 @@
+(function() {
+  Tombloo.Service.extractors.register({
+    name : 'Photo - Nico Nico Seiga',
+    ICON : 'http://seiga.nicovideo.jp/favicon.ico',
+    URL  : 'http://seiga.nicovideo.jp/',
+    check : function(ctx) {
+      if (!ctx.onLink) {
+        return false;
+      }
+      return ctx.href.indexOf('//seiga.nicovideo.jp/seiga/') > -1;
+    },
+    extract : function(ctx) {
+      ctx.target.src = ctx.link.href;
+      return Tombloo.Service.extractors['Photo'].extract(ctx);
+    }
+  }, 'Photo', false);
+})();
