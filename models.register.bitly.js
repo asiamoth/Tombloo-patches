b4c81c9600861dbd4e6a1fc7cce1474a1264c037
asiamoth <asiamoth@gmail.com>
2011-07-27 13:12:59 +0900

    bit.ly の API_KEY と USER を自分の登録情報と入れ換えるパッチ
    * 新規に追加した

---
+++ models.register.bitly.js
@@ -0,0 +1,6 @@
+models.register(update({}, models['bit.ly'], {
+  name    : 'bit.ly',
+  API_KEY : 'R_50d255ce8c98ab9814c0daea25a8150d',
+  API_KEY : '*_********* your API_KEY *********',
+  USER    : '*** your User Name **',
+}));
